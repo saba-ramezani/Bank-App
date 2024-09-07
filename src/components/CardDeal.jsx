b33f08195a30658566133eeca8d90a39f1e51d05
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 15:43:30 +0330

    added cardDeal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles, { layout } from "../style";
+import Button from "./Button";
+import { card } from '../assets';
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={`${layout.sectionImg}`}>
+      <img src={card} alt="" />
+    </div>
+  </section>
   )
 }
 
