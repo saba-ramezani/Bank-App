1a3a40e2fd440d0709c97b0d3853929371f5e48f
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 15:35:33 +0330

    added business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { features } from "../constants";
 import styles, { layout } from "../style";
+import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
@@ -18,11 +18,28 @@ const FeatureCard = ({ icon, title, content, index }) => (
   </div>
 );
 
+const Business = () =>  (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
 
-const Business = () => {
-  return (
-    <div>Business</div>
-  )
-}
+      <Button styles={`mt-10`} />
+    </div>
 
-export default Business
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Business;
