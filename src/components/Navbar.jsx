e0de1decd2e9c9d706070c67da22145e5108a70f
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 13:56:39 +0330

    added initial design of navbar for larger devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {close, logo, menu} from '../assets'
+import {navLinks} from '../constants'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
+            <a href={`${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        ></div>
+      </div>
+    </nav>
   )
 }
 
