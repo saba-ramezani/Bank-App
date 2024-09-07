da4e0b3188338d9f10a56caaf447d71e92a610ec
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 15:27:00 +0330

    added get started button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    Get Started
+  </button>
+);
 
-export default Button
+export default Button;
