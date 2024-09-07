6987f42194573ae8d6c4952e3baf80463dba6886
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 15:35:17 +0330

    added FeatureCard component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,4 +1,23 @@
 import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
 
 const Business = () => {
   return (
