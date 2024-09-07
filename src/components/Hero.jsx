f87e49f27f99d120eaac13bcaaf1cb838f38009e
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 14:24:36 +0330

    added discount part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+<section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
