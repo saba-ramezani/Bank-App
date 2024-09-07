88af8b03e0323ac70705678ba14fe1c11065ee53
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 14:28:35 +0330

    added heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,13 @@ const Hero = () => {
             <span className="text-white">1 Month</span> Account
           </p>
         </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "} <br />
+            Payment Method.
+          </h1>
+        </div>
       </div>
     </section>
   )
