d6bf2e5cfce0a64ea597aa635f646f156d0ecdd8
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 14:12:17 +0330

    added navbar menu for mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,18 @@ const Navbar = () => {
           className={`${
             !toggle ? "hidden" : "flex"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-        ></div>
+        >
+          <ul className='list-none flex justify-end items-center flex-1 flex-col'>
+            {navLinks.map((nav, index) => (
+              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]
+               text-white ${index === navLinks.length - 1 ? 'mb-0' : 'mb-5'}`}>
+                <a href={`${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   )
