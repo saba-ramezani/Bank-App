9e83627b483eeb0932c60ba6a46198589bcbbad4
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 13:27:14 +0330

    added initial components

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedbackCard = () => {
+  return (
+    <div>FeedbackCard</div>
+  )
+}
+
+export default FeedbackCard
---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetStarted = () => {
+  return (
+    <div>GetStarted</div>
+  )
+}
+
+export default GetStarted
